e48b5d8e686aac2020ec7ff37d9a0b1565eaa1ed
Debbie Cravey & Matt Keene <student@epicodus.com>
2015-01-14 11:30:47 -0800

    add new mods to spec and lib files

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,5 +1,45 @@
 class Vehicle
-  define_method(:initialize)  do
-    #code
+  @@vehicles = []
+
+  define_method(:initialize)  do |make, model, year|
+    @make = make
+    @model = model
+    @year = year
+  end
+
+  define_method(:make) do
+    @make
+  end
+
+  define_method(:model) do
+    @model
+  end
+
+  define_method(:year) do
+    @year
+  end
+
+  define_method(:save) do
+    @@vehicles.push(self)
+  end
+
+  define_singleton_method(:all) do
+    @@vehicles
+  end
+
+  define_singleton_method(:clear) do
+    @@vehicles = []
+  end
+
+  define_method(:age) do
+    current_year = Time.new().year()
+    current_year.-(@year)
+  end
+
+  define_method(:worth_buying?) do
+    american_cars = ["Chrysler", "Ford", "GM"]
+    american = american_cars.include?(@make)
+    new_enough = self.age().<=(15)
+    american.&(new_enough)
   end
 end
--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -2,15 +2,53 @@ require('rspec')
 require('vehicle')
 
 describe(Vehicle) do
-  # before() do
-  #     Vehicle.clear()
-  # end
+  before() do
+      Vehicle.clear()
+  end
 
   describe('#make') do
     it("returns the make of the vehicle") do
       test_vehicle = Vehicle.new("Toyota", "Tundra", 2000)
       test_vehicle.save()
-      expect(test_vehicle.make()).to(eq("Toyota"))
+      expect(test_vehicle.make()).to eq("Toyota")
     end
-end
+  end
+
+  describe('#model') do
+    it("returns the model of the vehicle") do
+      test_vehicle = Vehicle.new("Toyota", "Tundra", 2000)
+      test_vehicle.save()
+      expect(test_vehicle.model()).to eq("Tundra")
+    end
+  end
+
+  describe('#year') do
+    it("returns the year of the vehicle") do
+      test_vehicle = Vehicle.new("Toyota", "Tundra", 2000)
+      test_vehicle.save()
+      expect(test_vehicle.year()).to eq(2000)
+    end
+  end
+
+  describe("#save") do
+    it("adds a vehicle to the dealership inventory") do
+      test_vehicle = Vehicle.new("Toyota", "Tundra", 2000)
+      test_vehicle.save()
+      expect(Vehicle.all()).to eq([test_vehicle])
+    end
+  end
+
+  describe("#age") do
+    it("returns the vehicle age") do
+      test_vehicle = Vehicle.new("Toyota", "Tundra", 2000)
+      expect(test_vehicle.age()).to eq(15)
+    end
+  end
+
+    describe("#worth_buying?") do
+      it("returns false if the car is not American made and less than 15 years old") do
+      test_vehicle = Vehicle.new("Toyota", "Tundra", 2000)
+      expect(test_vehicle.worth_buying?()).to eq(false)
+    end
+  end
 end
